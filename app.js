b8505c8d8367bdddd292e8f6b666045518301645
Swapnilss53 <swapnilshinde532001@gmail.com>
2022-09-26 14:33:26 +0530

    modified

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const app = express();
+
+app.use(bodyParser.urlencoded({extended:true}));
+
+app.get("/",function(req,res){
+    res.send("<h1>Hello everyone website started</h1>");
+})
+
+app.listen(3000,function(){
+    console.log("server started");
+})
