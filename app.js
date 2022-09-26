a2e4430537178d540f0cc42ed358bf9c60305c54
Swapnilss53 <swapnilshinde532001@gmail.com>
2022-09-26 14:47:59 +0530

    Added port

--- app.js
+++ app.js
@@ -9,6 +9,6 @@ app.get("/",function(req,res){
     res.send("<h1>Hello everyone website started</h1>");
 })
 
-app.listen(3000,function(){
+app.listen(3000 || process.env.PORT,function(){
     console.log("server started");
 })
